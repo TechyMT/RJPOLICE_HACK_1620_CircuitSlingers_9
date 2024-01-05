92d90f7927a9c0fc1247dbbb260334f9e5e2c20f
Mustafa <100032108+TechyMT@users.noreply.github.com>
2024-01-06 02:21:23 +0530

    Delete unused images and packages, add providers and navbar components

---
+++ client/app/components/Navbar/index.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <div className="flex w-full bg-white text-black">
+      <div className="flex mx-auto">
+        <div className="flex">LOGO</div>
+        <div className="flex">Number</div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
