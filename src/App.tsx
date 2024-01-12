abb2452dcd94e9340914a418a0a788bf3ae3fa7a
Mustafa <100032108+TechyMT@users.noreply.github.com>
2024-01-13 01:49:34 +0530

    removed loading state

--- src/App.tsx
+++ src/App.tsx
@@ -9,11 +9,11 @@ import routes from './routes';
 const DefaultLayout = lazy(() => import('./layout/DefaultLayout'));
 
 function App() {
-  const [loading, setLoading] = useState<boolean>(true);
+  const [loading, setLoading] = useState<boolean>(false);
 
-  useEffect(() => {
-    setTimeout(() => setLoading(false), 1000);
-  }, []);
+  // useEffect(() => {
+  //   setTimeout(() => setLoading(false), 1000);
+  // }, []);
 
   return loading ? (
     <Loader />
@@ -31,17 +31,7 @@ function App() {
           <Route index element={<ECommerce />} />
           {routes.map((routes, index) => {
             const { path, component: Component } = routes;
-            return (
-              <Route
-                key={index}
-                path={path}
-                element={
-                  <Suspense fallback={<Loader />}>
-                    <Component />
-                  </Suspense>
-                }
-              />
-            );
+            return <Route key={index} path={path} element={<Component />} />;
           })}
         </Route>
       </Routes>
