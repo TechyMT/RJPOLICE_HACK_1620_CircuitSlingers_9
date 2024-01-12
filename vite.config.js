950956154898736433e9b132b15039332e2031fd
Mustafa <100032108+TechyMT@users.noreply.github.com>
2024-01-13 02:15:30 +0530

    Update base path in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,11 +2,10 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
-export default defineConfig(({ command }) =>
-{
+export default defineConfig(({ command }) => {
   const isProduction = command === 'build';
   return {
     plugins: [react()],
-    base: isProduction ? '/dist/' : '/dist/',
+    base: isProduction ? '/' : './', // Change the base path for development
   };
 });
