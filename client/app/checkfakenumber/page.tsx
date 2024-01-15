20992c041c133d6ea94f708f0777339b8d008c52
riya <riyawani26@gmail.com>
2024-01-15 18:21:17 +0530

    pages made responsive

--- client/app/checkfakenumber/page.tsx
+++ client/app/checkfakenumber/page.tsx
@@ -5,7 +5,6 @@ import fakeNumberImage from "../assets/images/fraud_1.jpg";
 import { publicUrl } from "../utils/publicURL";
 import { Button, Input, Spinner } from "@nextui-org/react";
 import Heading from "../components/Heading";
-import { set } from "firebase/database";
 
 const FakeNumberChecker = () => {
   const [phoneNumber, setPhoneNumber] = useState("");
@@ -13,50 +12,26 @@ const FakeNumberChecker = () => {
   const [loading, setLoading] = useState(false);
 
   const checkNumber = async () => {
-    // Basic logic to determine if a number is fake or not (you can replace this with your own logic)
-    // const fakePrefixes = ['555', '666', '123']; // Example fake prefixes
-    // const isFakeNumber = fakePrefixes.some((prefix) => phoneNumber.startsWith(prefix));
-    // setIsFake(isFakeNumber);
     setLoading(true);
     const response = await fetch(
       `${publicUrl()}/fraud_search/numbers/${phoneNumber}`
     );
     const { isFraud: isFakeNumber } = await response.json();
 
-    console.log(isFakeNumber);
     setIsFake(isFakeNumber);
     setLoading(false);
   };
 
   return (
-    <div
-      style={{
-        textAlign: "center",
-        marginTop: "50px",
-        fontFamily: "Arial, sans-serif",
-      }}
-    >
+    <div className="container mx-auto mt-8 mb-8 md:mb-48 p-4 text-center">
       <div className="my-4">
         <Heading>Fake Number Checker</Heading>
       </div>
-      <div
-        style={{
-          display: "flex",
-          flexDirection: "column",
-          alignItems: "center",
-        }}
-      >
-        <div
-          style={{
-            maxWidth: "300px",
-            margin: "20px",
-            borderRadius: "8px",
-            boxShadow: "0 4px 8px rgba(0, 0, 0, 0.1)",
-          }}
-        >
-          <Image src={fakeNumberImage} alt="RBI guidelines" width={1920} />
+      <div className="flex flex-col items-center">
+        <div className="max-w-[300px] mx-auto my-4 rounded-md shadow-md overflow-hidden">
+          <Image src={fakeNumberImage} alt="RBI guidelines" width={1920} height={1080} />
         </div>
-        <div className="w-96">
+        <div className="w-full md:w-96">
           <Input
             type="text"
             value={phoneNumber}
@@ -71,28 +46,20 @@ const FakeNumberChecker = () => {
           onClick={checkNumber}
           color="primary"
           size="lg"
-          className="my-12"
+          className="my-4 md:my-12"
         >
           Check
         </Button>
         {loading && (
-          <p
-            style={{
-              margin: "20px",
-              fontSize: "1.2em",
-              color: isFake ? "red" : "green",
-            }}
-          >
+          <p className="my-4 text-lg">
             <Spinner color="primary" size="lg" />
           </p>
         )}
         {isFake !== null && !loading && (
           <p
-            style={{
-              margin: "20px",
-              fontSize: "1.2em",
-              color: isFake ? "red" : "green",
-            }}
+            className={`my-4 text-lg ${
+              isFake ? "text-red-500" : "text-green-500"
+            }`}
           >
             The number {phoneNumber} is {isFake ? "fake" : "not fake"}.
           </p>
