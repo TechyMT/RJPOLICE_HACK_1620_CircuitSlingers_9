640e6b299d9d0d17276b551611552177bf69d97f
Mustafa <100032108+TechyMT@users.noreply.github.com>
2024-01-18 10:55:13 +0530

    updated url

--- src/utils/publicUrl.ts
+++ src/utils/publicUrl.ts
@@ -1,4 +1,4 @@
 export const publicUrl = () => {
-    return "https://rjpolicehack.onrender.com/api"
-    // return "http://192.168.137.1:8080/api"
+    // return "https://rjpolicehack.onrender.com/api"
+    return "http://192.168.137.1:8080/api"
 }
